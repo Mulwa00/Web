904c417ed8f00e0eb2e44ebc531f747fb8319be9
Patrick Mulwa <patrickmulwa012@gmail.com>
2024-07-30 19:31:50 +0300

    Update

--- send.php
+++ send.php
@@ -6,40 +6,52 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-if (isset($_POST["send"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send"])) {
     $mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'patrickmulwa012@gmail.com';
-    $mail->Password = 'fwww sltz fltu lcry';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'patrickmulwa012@gmail.com';
+        $mail->Password = 'fwww sltz fltu lcry';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
 
-    $mail->setFrom('patrickmulwa012@gmail.com');
+        // Recipients
+        $mail->setFrom('patrickmulwa012@gmail.com');
+        $mail->addAddress($_POST["email"]);
 
-    $mail->addAddress($_POST["email"]);
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Moving Service Request';
+        $mail->Body = "
+            <h1>Service Request Details</h1>
+            <p>First Name: " . htmlspecialchars($_POST['firstName']) . "</p>
+            <p>Last Name: " . htmlspecialchars($_POST['lastName']) . "</p>
+            <p>Service: " . htmlspecialchars($_POST['service']) . "</p>
+            <p>Moving From: " . htmlspecialchars($_POST['movingFrom']) . "</p>
+            <p>Moving To: " . htmlspecialchars($_POST['movingTo']) . "</p>
+            <p>Moving Date: " . htmlspecialchars($_POST['movingDate']) . "</p>
+            <p>Phone Number: " . htmlspecialchars($_POST['phoneNumber']) . "</p>
+            <p>Email: " . htmlspecialchars($_POST['email']) . "</p>
+        ";
 
-    $mail->isHTML(true);
-
-    $mail->firstName = ($_POST['firstName']);
-    $mail->lastName = ($_POST['lastName']);
-    $mail->service = ($_POST['service']);
-    $mail->movingFrom = ($_POST['movingFrom']);
-    $mail->movingTo = ($_POST['movingTo']);
-    $mail->movingDate = ($_POST['movingDate']);
-    $mail->phoneNumber = ($_POST['phoneNumber']);
-    $mail->email = ($_POST['email']);
-
-    $mail->submit();
-
-    echo
-    "
-    <script>
-    alert('Sent Successfully');
-    document.location.href = 'services.html';
-    </script>
-    ";
+        $mail->send();
+        echo "
+        <script>
+        alert('Sent Successfully');
+        document.location.href = 'services.html';
+        </script>
+        ";
+    } catch (Exception $e) {
+        echo "
+        <script>
+        alert('Message could not be sent. Mailer Error: {$mail->ErrorInfo}');
+        document.location.href = 'services.html';
+        </script>
+        ";
+    }
 }
 ?>
